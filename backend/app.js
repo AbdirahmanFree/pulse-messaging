cdb14e9b239fb4c210bb3a63f1361da4a480b9f8
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-07-11 00:00:43 -0400

    initialiized and connected to socket

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import router from './routes/routes.js'
 import cors from "cors"
+import { Server } from 'socket.io'
+import {createServer} from 'http'
 
 
 const app = express()
-// allows app to parse json payloads and populates req.body
+const server = createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:5173"
+    }
+})
+
 app.use(express.json())
-// parses html from form responses
 app.use(express.urlencoded({extended: true}))
 app.use(cors({origin:"http://localhost:5173"}))
 
 app.use(router)
+io.on("connection", (socket)=>{
+    console.log("user connected")
+})
 
-app.listen(5000,()=>{
+server.listen(5000,()=>{
     console.log("Listening on port 5000")
 })
